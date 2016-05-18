56a332be0f46a6f62f446a140bd4625366dc2d88
Avadanei Danut <avadanei.d@pg.com>
2016-05-18 19:18:25 +0300

    Refactored builder class, added dynamic where clauses and escaping

--- src/Builder.php
+++ src/Builder.php
@@ -2,164 +2,266 @@
 
 namespace LdapQuery;
 
+
+use BadMethodCallException;
+use Closure;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+use LdapQuery\Exceptions\GrammarException;
+use LdapQuery\Filter\FilterFactory;
+use LdapQuery\Group\Group;
+use LdapQuery\Group\GroupFactory;
+
 class Builder
-{	
+{   
+    /**
+     * Filter group
+     * 
+     * @var Group
+     */
+    protected $group;
 
-	/**
-	 * Filter group
-	 * 
-	 * @var \LdapQuery\FilterGroup
-	 */
-	protected $group;
+    /**
+     * Accepted dynamicWhere methods names
+     * 
+     * @var array
+     */
+    protected $dynamicWheres = [
+        'whereRaw',
+        'orWhereRaw',
+        'whereNot',
+        'whereNotRaw',
+        'orWhereNot',
+        'orWhereNotRaw',
+        'whereBegins',
+        'whereBeginsRaw',
+        'whereBeginsNot',
+        'whereBeginsNotRaw',
+        'orWhereBegins',
+        'orWhereBeginsRaw',
+        'orWhereBeginsNot',
+        'orWhereBeginsNotRaw',
+        'whereEnds',
+        'whereEndsRaw',
+        'whereEndsNot',
+        'whereEndsNotRaw',
+        'orWhereEnds',
+        'orWhereEndsRaw',
+        'orWhereEndsNot',
+        'orWhereEndsNotRaw',
+        'whereLike',
+        'whereLikeRaw',
+        'whereLikeNot',
+        'whereLikeNotRaw',
+        'orWhereLike',
+        'orWhereLikeRaw',
+        'orWhereLikeNot',
+        'orWhereLikeNotRaw',
+    ];
 
-	/**
-	 * Create a new instance of the Builder
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		// Instantiate the main filter group. This will get nested as more groups
-		// will be spawned.
-		$this->group = new FilterGroup;
-	}
+    /**
+     * Create a new instance of the Builder
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // Create or store the main filter group. This will get nested as more groups
+        // will be spawned.
+        $this->group = GroupFactory::build();
+    }
 
-	/**
-	 * Create a new instance of the Builder and return it.
-	 * 
-	 * @return \LdapQuery\Builder
-	 */
-	public static function create()
-	{
-		return new Builder;
-	}
+    /**
+     * Convert Group object to a string, LDAP valid query group.
+     * 
+     * @return string
+     */
+    public function stringify()
+    {
+        return $this->group->stringify();
+    }
 
-	/**
-	 * Add a where clause to the LDAP Query
-	 * 
-	 * @param  string|\Closure   $attribute
-	 * @param  string|array|null $operator
-	 * @param  string|array|null $value
-	 * @param  string            $logical
-	 *
-	 * @return $this
-	 *
-	 * @throws \LdapQuery\Exceptions\GrammarException
-	 */
-	public function where($attribute, $operator = null, $value = null, $logical = '&')
-	{
-		// If the current group operator is different from the requested
-		// spawn a new group and nest the current one
-		if ($this->group->getOperator() !== $logical){
-			$group = new FilterGroup($logical);
+    /**
+     * Convert Builder object to array.
+     * 
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->group->toArray();
+    }
 
-			if (count($group->getFilters()) === 1) {
-				$group->push($this->group->getFilters()[0]);
-			} else {
-				$group->push($this->group);
-			}
-			
-			$this->group = $group;
-		}
+    /**
+     * Add a where clause to the LDAP Query. Defaulted to & logical, acts like a andWhere.
+     * 
+     * @param  string|Closure    $attribute
+     * @param  string|array|null $operator
+     * @param  string|array|null $value
+     * @param  string|null       $wildcard
+     * @param  bool              $escape
+     * @param  string            $logical
+     *
+     * @return $this
+     *
+     * @throws GrammarException
+     */
+    public function where($attribute, $operator = null, $value = null, $wildcard = null, $escape = true, $negation = false, $logical = '&')
+    {
+        // If value is null, we assume that value is allocated to $operator
+        // and the we want default operator (=)
+        if ($value === null) {
+            $value = $operator;
+            $operator = '=';
+        }
 
-		if ($attribute instanceof \Closure) {
-			$builder = new Builder;
-			$attribute($builder);
-			$this->group->push($builder->getGroup());
-			return $this;
-		}
+        // If value is an array, use a closure to generate a or group with all the
+        // values.
+        if (is_array($value)) {
+            return $this->where(function($builder) use ($attribute, $value, $wildcard, $escape){
+                foreach ($value as $v) {
+                    $builder->orWhere($attribute, '=', $v, $wildcard, $escape);
+                }
+            }, null, null, null, null, $negation, $logical);
+        }
 
-		if ($value === null) {
-			$value = $operator;
-			$operator = '=';
-		}
+        // Solve context accordingly to the rules.
+        $group = $this->solveContext($logical, $negation);
 
-		if (is_array($value)) {
-			return $this->where(function($builder) use ($attribute, $operator, $value){
-				foreach($value as $v) {
-					$builder->where($attribute, $operator, $v, '|');
-				}
-			}, null, null, $logical);
-		}
+        // If the attribute is a closure then we assume that we want to create a new context
+        // and push it into the current group
+        if ($attribute instanceof Closure) {
+            $closureBuilder = new self();
+            $attribute($closureBuilder);
+            $group->push($closureBuilder->group);
+            return $this;
+        }
 
-		$this->group->push(Filter::create($attribute, $operator, $value));
-		return $this;
-	}
+        $group->push(FilterFactory::build($attribute, $value, $operator, $wildcard, $escape));
+        return $this;
+    }
 
-	/**
-	 * Add a or where clause to the LDAP Query
-	 * 
-	 * @param  string|\Closure   $attribute
-	 * @param  string|array      $operator
-	 * @param  string|array|null $value
-	 *
-	 * @return $this
-	 *
-	 * @throws \LdapQuery\Exceptions\GrammarException
-	 */
-	public function orWhere($attribute, $operator = null, $value = null)
-	{
-		return $this->where($attribute, $operator, $value, '|');
-	}
+    /**
+     * Add a or where clause to the LDAP Query.
+     * 
+     * @param  string|Closure   $attribute
+     * @param  string|array|null $operator
+     * @param  string|array|null $value
+     * @param  string|null       $wildcard
+     * @param  bool              $escape
+     *
+     * @return $this
+     *
+     * @throws GrammarException
+     */
+    public function orWhere($attribute, $operator = null, $value = null, $wildcard = null, $escape = true, $negation = false)
+    {
+        return $this->where($attribute, $operator, $value, $wildcard, $escape, $negation, '|');
+    }
 
-	/**
-	 * Add a or where clause to the LDAP Query
-	 * 
-	 * @param  string|\Closure   $attribute
-	 * @param  string|array      $operator
-	 * @param  string|array|null $value
-	 *
-	 * @return $this
-	 *
-	 * @throws \LdapQuery\Exceptions\GrammarException
-	 */
-	public function andWhere($attribute, $operator = null, $value = null)
-	{
-		return $this->where($attribute, $operator, $value, '&');
-	}
+    /**
+     * Handle dynamic where clauses to the LDAP Query. 
+     * 
+     * @param  string $method
+     * @param  string|Closure $attribute
+     * @param  string|array|null $operator
+     * @param  string|array|null $value
+     * 
+     * @return $this
+     *
+     * @throws GrammarException
+     * @throws InvalidArgumentException
+     */
+    protected function dynamicWhere($method, $attribute, $operator = null, $value = null)
+    {
+        $options = explode('_', snake_case($method));
 
-	/**
-	 * Return Builder Group
-	 * 
-	 * @return \LdapQuery\FilterGroup
-	 */
-	public function getGroup()
-	{
-		return $this->group;
-	}
+        if (Str::startsWith($method, 'where')) {
+            $method = 'where';
+        } else {
+            $method = 'orWhere';
+        }
 
-	/**
-	 * Print prettified version of the query
-	 * 
-	 * @return void
-	 */
-	public function prettify()
-	{
-		$pretified = $this->toArray();
+        // Add escape option.
+        $escape = !in_array('raw', $options);
 
-		foreach ($pretified as $value) {
-			print($value . PHP_EOL);
-		}
-	}
+        // Add wildcard option.
+        if (in_array('begins', $options)) {
+            $wildcard = 'begins';
+        } elseif (in_array('ends', $options)) {
+            $wildcard = 'ends';
+        } elseif (in_array('like', $options)) {
+            $wildcard = 'like';
+        } else {
+            $wildcard = null;
+        }
 
-	/**
-	 * Prettify the query into array
-	 * 
-	 * @return array
-	 */
-	public function toArray($print = false)
-	{
-		return $this->group->toArray();
-	}
+        if ($wildcard !== null && $attribute instanceof Closure) {
+            throw new InvalidArgumentException("Dynamic where clauses don't support wildcard operator if attribute is a Closure");
+        }
 
-	/**
-	 * Cast object to string
-	 * 
-	 * @return string
-	 */
-	public function __toString()
-	{
-		return $this->group->__toString();
-	}
+        $negation = in_array('not', $options);
 
+        return $this->{$method}($attribute, $operator, $value, $wildcard, $escape, $negation);
+    }
+
+    /**
+     * Solve the group context. 
+     * 
+     * @param string $logical
+     * @param bool   $negation
+     * 
+     * @return Builder
+     */
+    protected function solveContext($logical, $negation)
+    {
+        if ($this->group->getOperator() !== $logical){
+            // If the current group operator is different from the requested
+            // spawn a new group and nest the current one
+            $group = GroupFactory::build($logical);
+            $this->group->nest($group);
+            $this->group = $group;
+        }
+
+        if ($negation) {
+            // Create a new ! group, push it without clone and return to
+            // build query on it.
+            $context = GroupFactory::build('!');
+            $group = GroupFactory::build();
+            $context->push($group, false);
+            $this->group->push($context, false);
+            return $group;
+        }
+        return $this->group;
+    }
+    
+    /**
+     * Dynamically cast the object to string.
+     * 
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->stringify();
+    }
+
+    /**
+     * Handle dynamic method calls into the method.
+     *
+     * @param string  $method
+     * @param array   $parameters
+     * 
+     * @return mixed
+     *
+     * @throws BadMethodCallException
+     */
+    public function __call($method, $parameters)
+    {
+        if (in_array($method, $this->dynamicWheres)) {
+            return $this->dynamicWhere($method, ...$parameters);
+        }
+
+        $className = static::class;
+
+        throw new BadMethodCallException("Call to undefined method {$className}::{$method}()");
+    }
 }
